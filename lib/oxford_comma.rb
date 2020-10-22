fe04dee567d71a403ba9ea2cb125c125eadde0da
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-10-22 15:49:19 -0400

    commit tree 36a8cec28d5628a3a7d884472a8089a82c3a7691

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+    if array.length <= 2 
+        return array.join(" and ")
+    else
+        last_elm = array.pop
+        return array.join(", ") << ", and #{last_elm}"
+    end
 end
